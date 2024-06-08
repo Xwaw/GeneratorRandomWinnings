c744134ad116dff6c6026706d008e2a90ff8cf2e
Xwaw <93494637+Xwaw@users.noreply.github.com>
2024-06-09 00:27:54 +0200

    hell (o)

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 
 public class Main extends JFrame {
-    // There will probably be options here if I need them in the future
+    // There will probably be options here if I need them in the future.
     public static void main(String[] args) {
         Window window = new Window();
     }
